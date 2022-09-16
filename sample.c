d68a795e3731c5b3e662064a2737c87ef6313234
214g5a0511 <112165146+214g5a0511@users.noreply.github.com>
2022-09-16 15:57:32 +0530

    created sample.c

---
+++ sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: P.Srinath
+   Roll No: 214g5a0511
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 16-09-2022
+*/
